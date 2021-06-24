874b52362c2c20a20b15dc0376a4035a14e55b51
Matt Olenik <matt@olenik.me>
2021-06-23 18:35:42 -0700

    File restructure, remove buggy LOGGED

--- e2e-tests/vagrant-box/src/install-guest-additions.sh
+++ e2e-tests/vagrant-box/src/install-guest-additions.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -exuo pipefail
 
 MOUNT_POINT="${MOUNT_POINT:-/mnt/vb}"
-ISO_URL="${ISO_URL:-https://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso}"
+ISO_URL="${ISO_URL:-"https://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso"}"
 
 main() {
+    echo "Installing VBoxGuestAdditions for VBox version '$VBOX_VERSION'"
     apt-get -y install build-essential dkms pve-headers
     iso_path="/tmp/vbga.iso"
     curl -sSLo "$iso_path" "$ISO_URL"
