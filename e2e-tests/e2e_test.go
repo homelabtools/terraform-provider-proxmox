ac6a24caf2c201884a3c9c9b75de89a6e18e8f34
Matt Olenik <olenikm@gmail.com>
2021-07-18 11:43:19 -0700

    Fix test teardown order

--- e2e-tests/e2e_test.go
+++ e2e-tests/e2e_test.go
@@ -8,13 +8,17 @@ import (
 
 var endpoint = "http://localhost:8000"
 
-func TestMain(t *testing.T) {
-	tf := fixtures.NewTerraformTestFixture(t, "cases/simple", "1.0.1", endpoint, "root@pam", "proxmox")
-	defer tf.TearDown()
+// TODO: Have Proxmox fixture optionally created during suite setup in addition to test setup.
 
+func TestMain(t *testing.T) {
+	// Proxmox fixture should come first. Specifically, Proxmox TearDown should happen *after* TF TearDown.
+	// Otherwise TF TearDown fails because the VM no longer exists.
 	var pve *fixtures.ProxmoxTestFixture
 	defer func() { pve.TearDown() }()
 	pve = <-fixtures.NewProxmoxTestFixture(t, fixtures.ProxmoxTestFixtureOptions{})
+
+	tf := fixtures.NewTerraformTestFixture(t, "cases/simple", "1.0.1", endpoint, "root@pam", "proxmox")
+	defer tf.TearDown()
 
 	tf.Init().Apply()
 }
